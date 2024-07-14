f107dd1a0d339fc1c71e2143c3ad0492b9475762
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-07-14 21:26:00 +0900

    fix: 不要マークアップ削除、github auth確認完了

--- src/components/Form/user-auth-form.tsx
+++ src/components/Form/user-auth-form.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+"use client";
+import { signIn } from "next-auth/react";
+import React, { useState } from "react";
 
 export default function UserAuthForm() {
+    const [isGitHubLoading, setIsGitHubLoading] = useState<boolean>(false);
     return (
         <div>
             <form>
-                <label htmlFor="email">email</label>
+                {/* <label htmlFor="email">email</label>
                 <input id="email" type="email" />
-                <button>login</button>
+                <button>login</button> */}
             </form>
-            <button>github login</button>
+            {isGitHubLoading ? <>loading</> : <>github</>}
+            <button
+                onClick={() => {
+                    setIsGitHubLoading(true);
+                    signIn("github");
+                }}
+            >
+                github login
+            </button>
         </div>
     );
 }
