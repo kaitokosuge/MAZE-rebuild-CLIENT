9b89aa7573e43948cec51dfa8326c4ce77fb8bfc
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-07-22 22:17:52 +0900

    add: layoutにヘッダー、サイドバー移動

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,5 @@
-import Header from "@/components/Header/Header";
-import Sidebar from "@/components/Sidebar/Sidebar";
 import React from "react";
 
 export default function page() {
-    return (
-        <main className="maze-subfont">
-            <div className="flex">
-                <Sidebar />
-                <Header />
-            </div>
-        </main>
-    );
+    return <main className="maze-subfont"></main>;
 }
